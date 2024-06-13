f9433a210e759bb4ff748ee525a7901e300844a4
EtlcT <agathe.badetti@epfedu.fr>
2024-06-13 17:53:26 +0200

    Implement unit tests for GetSpreadsheetData class function ; several bug fixes relative to those class function

--- src/extraction/retrieve_data.py
+++ src/extraction/retrieve_data.py
@@ -14,15 +14,16 @@ class GetSpreadsheetData:
     Class that read a spreadsheet and retrieve required data from it 
     """
 
-    def __init__(self, filename) -> None:
-        self.sheets_dict = self._read_spreadsheet(filename)
+    def __init__(self, filepath) -> None:
+        self.sheets_dict = self._read_spreadsheet(filepath)
         self.dbname = self.get_dbname()
+        self.keys = self._get_keys_df()
     
-    def _read_spreadsheet(self) -> dict:
+    def _read_spreadsheet(self, filepath) -> dict:
         """
         return a dictionnary containing as many dataframes as sheets in the original file
         """
-        return pd.read_excel(self.filename, sheet_name=None)
+        return pd.read_excel(filepath, sheet_name=None)
     
     def _regex_exclude_meta(self, text) -> bool:
         """
@@ -33,7 +34,7 @@ class GetSpreadsheetData:
         no_keys = re.search("(?i)^keys$", text)
         no_meta = re.search("(?i)meta\.", text)
         no_extra = re.search("(?i)extra_sheet\.", text)
-        return any(no_keys, no_meta, no_extra)
+        return any([no_keys, no_meta, no_extra])
 
 
     def get_datatables_list(self) -> list:
@@ -46,15 +47,17 @@ class GetSpreadsheetData:
         for sheet_name in self.sheets_dict:
             if(self._regex_exclude_meta(sheet_name) == False):
                 datatable_list.append(sheet_name)
+        return datatable_list
         
     def _get_keys_df(self) -> pd.DataFrame:
         """
         return a dataframe that contain rows from KEYS table
         where either Primary Key OR Foreign Key is not null
         """
-
-        return self.sheets_dict['KEYS'][self.sheets_dict['KEYS']['isPK'].notna()
+        keys_df = self.sheets_dict['KEYS'][self.sheets_dict['KEYS']['isPK'].notna()
                                             | self.sheets_dict['KEYS']['isFK'].notna()].iloc[:,:5]
+        
+        return keys_df.reset_index(drop=True)
     
     def get_dbname(self) -> str:
         """
---
+++ tests/test_extraction.py
@@ -0,0 +1,118 @@
+import unittest
+from parameterized import parameterized
+from unittest.mock import MagicMock
+
+import pandas as pd
+import numpy as np
+
+from src.extraction import retrieve_data
+
+def _read_spreadsheet_mock():
+    fields_A = ['Attribute_A1', 'Attribute_A2', 'Attribute_A3']
+    values_A = [
+        ['Ref_A1', 'value_1', 'some_value_1'],
+        ['Ref_A2', 'value_2', 'some_value_2'],
+                ]
+    table_A = pd.DataFrame(data=values_A, columns=fields_A)
+
+    fields_B = ['Attribute_A1', 'Attribute_B2', 'Attribute_B3']
+    values_B = [
+        ['Ref_A1', 'Ref_B1', 11],
+        ['Ref_A1', 'Ref_B2', 23],
+        ['Ref_A2', 'Ref_B1', 456],
+        ['Ref_A2', 'Ref_B2', 7],
+    ]
+    table_B = pd.DataFrame(data=values_B, columns=fields_B)
+
+    fields_C = ['Attribute_A1', 'Attribute_B1', 'Attribute_C1', 'Attribute_C2', 'Attribute_C3']
+    values_C = [
+        ['Ref_A1', 'Ref_B1', 'Ref_C1', 'value_a', 12],
+        ['Ref_A2', 'Ref_B1', 'Ref_C2', 'value_b', 42],
+        ['Ref_A1', 'Ref_B1', 'Ref_C3', 'value_b', 200]
+    ]
+    table_C = pd.DataFrame(data=values_C, columns=fields_C)
+
+    fields_KEYS = ['Table', 'Attribute', 'isPK', 'isFK', 'ReferenceTab']
+    values_KEYS = [
+        ['Table_A', 'Attribute_A1', 'Y', np.nan, np.nan],
+        ['Table_A', 'Attribute_A2', np.nan, np.nan, np.nan],
+        ['Table_A', 'Attribute_A3', np.nan, np.nan, np.nan],
+        ['Table_B', 'Attribute_A1', 'Y', 'Y', 'Table_A'],
+        ['Table_B', 'Attribute_B1', 'Y', np.nan, np.nan],
+        ['Table_B', 'Attribute_B2', np.nan, np.nan, np.nan],
+        ['Table_C', 'Attribute_A1', np.nan, 'Y', 'Table_A'],
+        ['Table_C', 'Attribute_B1', np.nan, 'Y', 'Table_B'],
+        ['Table_C', 'Attribute_C1', 'Y', np.nan, np.nan],
+        ['Table_C', 'Attribute_C2', np.nan, np.nan, np.nan],
+        ['Table_C', 'Attribute_C3', np.nan, np.nan, np.nan]
+    ]
+    table_KEYS = pd.DataFrame(data=values_KEYS, columns=fields_KEYS)
+
+    table_REF = pd.DataFrame(
+        columns=['key', 'value'],
+        data=[
+            ['Date', 2024],
+            ['Title', 'Template2024'],
+            ['DBfileName', '2022_Example#/Template_v2_1; ' ]
+        ]
+    )
+
+    return {
+        'Table_A': table_A,
+        'Table_B': table_B,
+        'Table_C': table_C,
+        'KEYS': table_KEYS,
+        'meta.REFERENCES': table_REF
+    }
+
+
+class TestExtraction(unittest.TestCase):
+    retrieve_data.GetSpreadsheetData._read_spreadsheet = MagicMock(return_value=_read_spreadsheet_mock())
+
+    @parameterized.expand([
+        ('meta.Something', True),
+        ('MeTa.CaseUnsensitive', True),
+        ('metacognitive', False),
+        ('KeYS', True),
+        ('not_keys_table', False),
+    ])
+
+    def test_regex_exclude_meta(self, text, regexMatch):
+        """Check that sheet that contains either 'meta', 'keys' or 'extra' 
+        matches the regex and so return True.
+        """
+
+        result = retrieve_data.GetSpreadsheetData._regex_exclude_meta(self, text=text)
+
+        self.assertEqual(result, regexMatch)
+
+
+    def test_get_datatables_list(self):
+        """Check that all tables that contains data are correctly retrieved"""
+        
+        getData = retrieve_data.GetSpreadsheetData('fakepath')
+        getData._read_spreadsheet = MagicMock(_read_spreadsheet_mock)
+        result = getData.get_datatables_list()
+
+        self.assertListEqual(result, ['Table_A', 'Table_B', 'Table_C'])
+
+    def test_get_keys_df(self):
+        """Check that we retrieve all attributes that are either defined as PK or FK and not the others"""
+
+        getData = retrieve_data.GetSpreadsheetData('fakepath')
+        getData._read_spreadsheet = MagicMock(_read_spreadsheet_mock)
+        result = getData._get_keys_df()
+
+        expected_result = pd.DataFrame(
+            columns= ['Table', 'Attribute', 'isPK', 'isFK', 'ReferenceTab'],
+            data= [
+                ['Table_A', 'Attribute_A1', 'Y', np.nan, np.nan],
+                ['Table_B', 'Attribute_A1', 'Y', 'Y', 'Table_A'],
+                ['Table_B', 'Attribute_B1', 'Y', np.nan, np.nan],
+                ['Table_C', 'Attribute_A1', np.nan, 'Y', 'Table_A'],
+                ['Table_C', 'Attribute_B1', np.nan, 'Y', 'Table_B'],
+                ['Table_C', 'Attribute_C1', 'Y', np.nan, np.nan]
+            ]
+        )
+
+        self.assertEqual(expected_result.equals(result), True)
