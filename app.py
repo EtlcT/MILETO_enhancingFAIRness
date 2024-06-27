b12be1b21b28abd41fe293776b16e4701e88bb75
EtlcT <agathe.badetti@epfedu.fr>
2024-06-27 16:37:25 +0200

    adding conf folder;update app.py

--- app.py
+++ app.py
@@ -5,33 +5,21 @@ from src.extraction.retrieve_data import GetSpreadsheetData
 from src.dbcreate.dbcreate import sqliteCreate
 from src.extraction.utils import checks_pipeline
 
-# def get_application_path():
-#     """
-#     Get the path where the application is running.
-#     """
-#     if getattr(sys, 'frozen', False):
-#         # Running in a bundle
-#         print('bundle')
-#         return os.path.dirname(sys.executable)
-#     else:
-#         # Running in a normal Python environment
-#         print('frozen')
-#         return os.path.dirname(os.path.abspath(__file__))
-
 def main():
-    print(os.getcwd())
     print('Welcome into Spreadsheet2sqlite\nEnter exit or Ctrl+C to quit.\n')
 
     while True:
         try:
-            print("Please provide the absolute pathfile to your spreadsheet\n")
-            user_input = input()
+            user_input = input("Please provide the absolute pathfile to your spreadsheet\n")
 
             if user_input=='exit':
                 print('Thank your for using Spreadsheet2Sqlite')
                 break
 
             else:
+                
+                output_dir = input("Please provide the output directory to save .sqlite .png and .pdf files\n")
+
                 file_path = os.path.normpath(user_input)
                 
                 data = GetSpreadsheetData(filepath=file_path)
@@ -44,12 +32,14 @@ def main():
                 ]
                 checks_pipeline(check_funcs_list)
 
-                dbCreate = sqliteCreate(data)
+                dbCreate = sqliteCreate(data, output_dir=output_dir)
                 dbCreate.create_db()
                 dbCreate.insert_data()
+                dbCreate.ddict_schema_create()
                 
         except Exception as e:
-            print("An error occured: ", e)
+            with open('logs.txt','a+') as logfile:
+                logfile.write(f"{e}\n")
 
 
 if __name__=='__main__':
