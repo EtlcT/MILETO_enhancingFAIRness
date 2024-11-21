72ff6669741ce796ce676e49122504c169585dd4
EtlcT <94692921+EtlcT@users.noreply.github.com>
2024-11-21 14:40:00 +0100

    Delete hooks directory
    remove unnecessary files

--- hooks/windows_hooks/hook-msvcrt.py
+++
@@ -1,6 +0,0 @@
-# hook-msvcrt.py
-from PyInstaller.utils.hooks import collect_submodules
-import sys
-
-if sys.platform.startswith('linux'):
-    hiddenimports = collect_submodules('msvcrt')
--- hooks/windows_hooks/hook-user32.py
+++
@@ -1,6 +0,0 @@
-# hook-user32.py
-from PyInstaller.utils.hooks import collect_submodules
-import sys
-
-if sys.platform.startswith('linux'):
-    hiddenimports = collect_submodules('user32')
